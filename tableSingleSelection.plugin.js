c6b5c6884f8b24b2e172b88abd68dc148385f556
Pete <pmckinney8806@gmail.com>
2014-06-17 03:03:51 -0500

    more plugin work

--- tableSingleSelection.plugin.js
+++ tableSingleSelection.plugin.js
@@ -8,7 +8,16 @@
   }
 
   $.fn.tableSingleSelection = function(){
+    this.config = $.extend(true,{},defaults,options);
 
+    var table = this.children('table');
+
+    if(table.length == 0 ) return this;
+
+    //other wise find the element in the table and
+    //apply the selected class to the containing row.
+
+    return this;
   };
 
 }(jQuery));
