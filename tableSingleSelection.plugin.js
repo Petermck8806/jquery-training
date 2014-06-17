35355bf1931a37b1cbe76cffdc1977f5b789e435
Pete <pmckinney8806@gmail.com>
2014-06-17 02:40:10 -0500

    Updated on click - books.html. Added jquery plugin.
    Updated books.html with on click method on the containing table.
    Started work on JQuery tableSingleSelection plugin.

---
+++ tableSingleSelection.plugin.js
@@ -0,0 +1,14 @@
+(function($){
+
+  var defaultSettings = {
+    selectedClass: 'selected',
+    targetSelector: 'a',
+    onSelected: $.noop,
+    isDisabled: $.noop
+  }
+
+  $.fn.tableSingleSelection = function(){
+
+  };
+
+}(jQuery));
